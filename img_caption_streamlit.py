2cdc7ed35591c6f8fc4de27384539ea5162e515e
Omar Ahmed <60344203+omarahmed19992@users.noreply.github.com>
2024-07-05 14:46:18 +0300

    Update img_caption_streamlit.py

--- img_caption_streamlit.py
+++ img_caption_streamlit.py
@@ -29,10 +29,10 @@ resnet_model = ResNet50()
 resnet_model = Model(inputs=resnet_model.inputs, outputs= resnet_model.layers[-2].output)
 
 @st.cache_resource
-def load_model():
-    model = load_model('best_model.h5')
-    return model 
-model = load_model()
+# def load_model():
+#     model = load_model('best_model.h5')
+#     return model 
+model = load_model('best_model.h5')
 
 # Load tokenizer 
 with open('tokenizer.pickle', 'rb') as handle:
